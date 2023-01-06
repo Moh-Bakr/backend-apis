0185e6b7f04b1a803c4be926b1850e7ff62bd329
Mohamed Bakr <mohamed.bakr@bld.ai>
2023-01-06 13:57:01 +0200

    remove swagger from development

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -10,13 +10,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<DataContext>();
 var app = builder.Build();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//    
+// }
 
 app.UseHttpsRedirection();
 
